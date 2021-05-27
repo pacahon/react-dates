e420736ee7309fc6e6ddb65aba0c74d14597bfee
pacahon <pacabest@gmail.com>
2021-05-27 21:24:29 +0300

    publish: [New] `DayPickerSingleDateController`: add `allowUnselect` prop that allows controlling whether unselecting selected days is possible
    generated from commit 6c2cb61c5e876fbdac72e6efed49c0bd64f06d6b

---
+++ main.7e5a16d3431ada7f48c7.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{437:function(n,o,c){c(438),c(593),n.exports=c(1210)},503:function(n,o){},593:function(n,o,c){"use strict";c.r(o);c(594),c(1207)}},[[437,1,2]]]);
